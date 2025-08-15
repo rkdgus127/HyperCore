f7cfb8bfabf4e9449552f15f4dd1e3d6e4e8c12c
nomusamo <kanghyun486111@gmail.com>
2025-08-15 21:39:38 +0900

    :sparkles: UPGRADE HYPERCORE

--- src/main/java/HyperCore/HyperCore.java
+++ src/main/java/HyperCore/HyperCore.java
@@ -5,8 +5,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HyperCore extends JavaPlugin {
 
+    private static HyperCore instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         HyperLoader.loadAll(this);
+    }
+
+    public static HyperCore getInstance() {
+        return instance;
     }
 }
